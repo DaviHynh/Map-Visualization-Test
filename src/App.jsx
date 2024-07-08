76cac4e04df93f6423b7bd8990ed411e8382d6de
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-07-08 16:37:58 -0400

    small structure change

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import MapDisplay from './components/MapDisplay'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='contentContainer'>
+
+
+    <MapDisplay />
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MapDisplay.jsx
@@ -0,0 +1,39 @@
+import '../App.css'
+
+function MapDisplay() {
+
+  return (
+    <>
+    <div className='mainArea'>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+    <div className='idk'></div>
+
+    <div className='idk'></div>
+
+    </div>
+    </>
+  )
+}
+
+export default MapDisplay
