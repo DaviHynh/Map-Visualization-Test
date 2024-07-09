d718445f002d165396b377827607fe43d4cc7971
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-07-09 06:03:00 -0400

    Updated grid

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -1,35 +1,30 @@
 import '../App.css'
 
+import { useState, useEffect } from 'react'
+
 function MapDisplay() {
+  const [grid, setGrid] = useState([]);
+
+  useEffect(() => {
+    // Generate 9 divs
+    const newDivs = [];
+    for (let i = 0; i < 81; i++) {
+      newDivs.push(<div key={i} className="child-div"></div>);
+    }
+    setGrid(newDivs);
+  }, []);
+
+  
+
+
+
+
 
   return (
     <>
     <div className='mainArea'>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
-    <div className='idk'></div>
+    {grid}
 
-    <div className='idk'></div>
 
     </div>
     </>
