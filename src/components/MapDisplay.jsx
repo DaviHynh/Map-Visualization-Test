6e9333d56dfe4ca3316df4ddfc0f912892befb18
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-07-13 15:31:15 -0400

    Added primative movement

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -1,59 +1,71 @@
 import '../App.css'
 import image from '../assets/react.svg'
 
-import { useState, useEffect} from 'react'
-import { motion } from 'framer-motion';
+import { useState, useEffect, useRef} from 'react'
+import { animate, motion, useAnimate } from 'framer-motion';
+
+
+  // To make movement consistent maybe I should make it relate to the viewport stuff?
+  // Whenever the window changes, I should update location of image
+  // I should also update movement based on cell size. (Normal = 55x55 pix) (smaller = 13x13 pix) so move 13 pixels instead of 55?
+  // also add throttling
+
 
 function MapDisplay() {
 
+  const [X, setX] = useState(0);
+  const [Y, setY] = useState(0);
+  
   const [grid, setGrid] = useState([]);
-
+  const [player, animatePlayer] = useAnimate();
 
   // Loads the initial grid.
   useEffect(() => {
     const newGrid = [];
-
+  
     for (let i = 0; i < 81; i++)
     {
-      newGrid.push(<div></div>);
+      newGrid.push(<div key={i}></div>);
     }
 
     setGrid(newGrid);
   }, []);
 
+  const keyPress = (e) =>
+  {
+    console.log("Key Input: " + e.key);
+    console.log(Y);
 
-  // Listens for key press events.
-  useEffect(() => {
-
-    function somefunction (event)
+    switch (e.key)
     {
-      if (event.key === 'w')
-      {
-        let lol = document.getElementById("main");
-
-        lol.translate(10, 10);
-
-        console.log('press w');
-      }
+      case 'w':
+        setY(Y - 30);
+        animatePlayer(player.current, {y: Y-30});
+        break;
+      case 'a':
+        setX(X - 30);
+        animatePlayer(player.current, {x: X-30});
+        break;
+      case 's':
+        setY(Y + 30);
+        animatePlayer(player.current, {y: Y+30});
+        break;
+      case 'd':
+        setX(X + 30);
+        animatePlayer(player.current, {x: X+30});
+        break;
     }
 
-    window.addEventListener('keydown', somefunction);
-
-    return () => {
-      window.removeEventListener('keydown', somefunction);
-    }
-  }, [])
+    console.log("Y After Change: " + Y);
+  }
 
   return (
     <>
-    <div className='mainArea'>
-
-
-    {/* <img id="main" src={image}/> */}
-    <motion.img src={image}/>
-
+    <div onKeyDown={keyPress} tabIndex={0} className='mainArea'>
 
     {grid}
+
+    <img src={image} ref={player}/>
 
     </div>
     </>
