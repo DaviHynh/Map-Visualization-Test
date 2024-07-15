da8a6ac04499212ddcf3067388553e1b07ac6cba
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-07-15 12:30:14 -0400

    Updated movement scaling

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -1,71 +1,77 @@
 import '../App.css'
 import image from '../assets/react.svg'
 
-import { useState, useEffect, useRef} from 'react'
-import { animate, motion, useAnimate } from 'framer-motion';
+import { useState, useEffect , useRef} from 'react'
+import { useAnimate } from 'framer-motion';
 
-
-  // To make movement consistent maybe I should make it relate to the viewport stuff?
   // Whenever the window changes, I should update location of image
-  // I should also update movement based on cell size. (Normal = 55x55 pix) (smaller = 13x13 pix) so move 13 pixels instead of 55?
   // also add throttling
 
-
-function MapDisplay() {
-
+function MapDisplay()
+{
   const [X, setX] = useState(0);
   const [Y, setY] = useState(0);
   
   const [grid, setGrid] = useState([]);
   const [player, animatePlayer] = useAnimate();
+  const gridRef = useRef();
 
   // Loads the initial grid.
   useEffect(() => {
     const newGrid = [];
+
+    let spawnTile = 40;
   
     for (let i = 0; i < 81; i++)
     {
-      newGrid.push(<div key={i}></div>);
+      if (i == spawnTile)
+      {
+        newGrid.push(<div key={i}><img src={image} ref={player}/></div>);
+      }
+      else
+      {
+        newGrid.push(<div key={i}></div>);
+      }
     }
 
-    setGrid(newGrid);
-  }, []);
+    console.log("Grid setting...");
 
+    setGrid(newGrid);
+  }, [player]);
+
+  // Handles Player Movement
   const keyPress = (e) =>
   {
     console.log("Key Input: " + e.key);
-    console.log(Y);
+
+    let dist = gridRef.current.children[0].getBoundingClientRect().width;
 
     switch (e.key)
     {
       case 'w':
-        setY(Y - 30);
-        animatePlayer(player.current, {y: Y-30});
+        setY(Y - dist);
+        animatePlayer(player.current, {y: Y-dist});
         break;
       case 'a':
-        setX(X - 30);
-        animatePlayer(player.current, {x: X-30});
+        setX(X - dist);
+        animatePlayer(player.current, {x: X-dist});
         break;
       case 's':
-        setY(Y + 30);
-        animatePlayer(player.current, {y: Y+30});
+        setY(Y + dist);
+        animatePlayer(player.current, {y: Y+dist});
         break;
       case 'd':
-        setX(X + 30);
-        animatePlayer(player.current, {x: X+30});
+        setX(X + dist);
+        animatePlayer(player.current, {x: X+dist});
         break;
     }
-
-    console.log("Y After Change: " + Y);
   }
 
   return (
     <>
-    <div onKeyDown={keyPress} tabIndex={0} className='mainArea'>
+    <div onKeyDown={keyPress} tabIndex={0} className='mainArea' ref={gridRef}>
 
     {grid}
-
-    <img src={image} ref={player}/>
 
     </div>
     </>
